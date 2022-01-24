84e73e3024124c2d4864a0c04717de7cf236f460
Bruno Guerra <gguerrabruno@outlook.com>
2022-01-23 22:25:07 -0300

    Adding docs

--- app/service/posts.service.ts
+++ app/service/posts.service.ts
@@ -1,5 +1,10 @@
 import { db } from "~/persistence/db";
 
+/**
+ * Creates a new post on the database
+ * @param formData The form data to create a post
+ * @returns Promise resolving to a new post
+ */
 export const createPost = async (formData: FormData) => {
   const data = Object.fromEntries(formData);
   const post = {
@@ -9,10 +14,19 @@ export const createPost = async (formData: FormData) => {
   return await db.post.create({ data: post })
 }
 
+/**
+ * Gets all the created posts
+ * @returns Promise resolving to an array of posts
+ */
 export const getAllPosts = async () => {
   return await db.post.findMany();
 }
 
+/**
+ * Deletes a post given it's id
+ * @param formData The form data that includes the post id
+ * @returns Promise, rejecting if no id is passed, or resolving to the deleted post if exists
+ */
 export const deletePost = async (formData: FormData) => {
   const id = formData.get("id");
   
@@ -27,6 +41,11 @@ export const deletePost = async (formData: FormData) => {
   });
 }
 
+/**
+ * Gets a single post given it's id
+ * @param id The post id
+ * @returns Promise resolving to the post if it exists
+ */
 export const getPostById = async (id: number) => {
   return db.post.findUnique({
     where: {
